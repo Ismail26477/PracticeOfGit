8bdca294b1b5c23e5aa1691715dfff9caa7865da
Ismail26477 <irsusherikh@gmail.com>
2023-12-22 21:53:09 +0530

    sixth commit

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-<<<<<<< HEAD
-def addition(a,b,e):
-    return a+b+e
-=======
 def addition(a,b,c):
     return a+b+c
->>>>>>> e14707ac6a54ee4a074ad60c9ec737e50a59c400
 
 def substraction(a,b):
     return a-b
+
+def multiplication(a,b):
+    return a*b
 
 def division(a,b):
     return a/b
